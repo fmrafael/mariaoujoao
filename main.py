dfb428933eb016ace09b0961c5cd85d2754c5168
Rafael Fernandes <rafaelfernandesmarcondes@gmail.com>
2021-06-09 13:40:04 +0000

    +deployment files

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 import pandas as pd
-from flask_bootstrap import Bootstrap
+ 
 
 engine = create_engine('sqlite:///babynames.db', echo = True)
 engine.connect()
 
 app = Flask(__name__)
-Bootstrap(app)
+ 
  
 
 
 df = pd.read_sql_table(
     'babynames',
     con=engine,
-    columns=['Nome','Gênero','Origem','Significado','Curiosidade']
+    columns=['Nome','Gênero','Origem','Significado','Curiosidade'] 
 )
+
 
 
 #r_set=cur.execute('''SELECT Nome,Gênero,Origem,Significado,Curiosidade from babynames ''');
@@ -25,7 +26,9 @@ df = pd.read_sql_table(
 
 @app.route("/", methods=("POST", "GET"))
 def home():
-  return render_template("index.html",tables=[df.to_html( classes='data', header="true",index="false")], titles=df.columns.values)
+  
+  return render_template("index.html",
+  tables=[df.to_html( classes='data', header="true")],titles=df.columns.values)
 
 
 if __name__ == '__main__':
